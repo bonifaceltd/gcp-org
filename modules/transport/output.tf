ec04abba99663b843a06c68af724a61f240c793c
mrkaleslie <mrkaleslie@gmail.com>
2021-02-15 18:51:23 +0000

    ensure output is map(string)

--- modules/transport/output.tf
+++ modules/transport/output.tf
@@ -1,8 +1,8 @@
 output "vpn" {
-    value = {
-        project = google_project.env.project_id
-        asn     = var.asn
-        gw      = google_compute_ha_vpn_gateway.this.name
-        cr      = google_compute_router.this
-    }
+  value = {
+    project = google_project.env.project_id
+    asn     = "645${var.asn}"
+    gw      = google_compute_ha_vpn_gateway.this.self_link
+    cr      = google_compute_router.this.self_link
+  }
 }
