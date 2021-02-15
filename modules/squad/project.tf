4754f2ca4c16d85bbb2db71605f03a3df3ead724
mrkaleslie <mrkaleslie@gmail.com>
2021-02-15 16:04:09 +0000

    Revert "change squad proj IDs 007 -> 0007"
    This reverts commit f47a874845799631c525602d21ea35035f1e70f9.

--- modules/squad/project.tf
+++ modules/squad/project.tf
@@ -1,6 +1,6 @@
 resource "google_project" "env" {
   name                = "${var.org}-${var.name}"
-  project_id          = "${var.org}-${var.name}-0007"
+  project_id          = "${var.org}-${var.name}-007"
   folder_id           = google_folder.env.name
   billing_account     = "01C4CA-74671D-650411"
   auto_create_network = false
