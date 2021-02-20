83993f4e024cd57480ddb858fe63e796c05c85c5
mrkaleslie <mrkaleslie@gmail.com>
2021-02-20 00:16:27 +0000

    remove redundant mock output attr
    replace with lookup which enables null default

--- modules/squad/vpn.tf
+++ modules/squad/vpn.tf
@@ -22,11 +22,11 @@ resource "google_compute_router" "this" {
 resource "google_compute_vpn_tunnel" "transport" {
   count                 = 2
   name                  = "${var.name}-tun${count.index}"
-  project               = var.transport["project"]
-  vpn_gateway           = var.transport["gw"]
+  project               = lookup(var.transport, "project", "")
+  vpn_gateway           = lookup(var.transport, "gw", "")
   peer_gcp_gateway      = google_compute_ha_vpn_gateway.this.self_link
   shared_secret         = "secret"
-  router                = var.transport["cr_link"]
+  router                = lookup(var.transport, "cr_link", "")
   vpn_gateway_interface = count.index
 }
 
@@ -35,7 +35,7 @@ resource "google_compute_vpn_tunnel" "squad" {
   name                  = "transport-${local.env}-tun${count.index}"
   project               = google_project.env.project_id
   vpn_gateway           = google_compute_ha_vpn_gateway.this.id
-  peer_gcp_gateway      = var.transport["gw"]
+  peer_gcp_gateway      = lookup(var.transport, "gw", "")
   shared_secret         = "secret"
   router                = google_compute_router.this.id
   vpn_gateway_interface = count.index
@@ -44,8 +44,8 @@ resource "google_compute_vpn_tunnel" "squad" {
 resource "google_compute_router_interface" "transport" {
   count      = 2
   name       = "${var.name}-iface${count.index}"
-  project    = var.transport["project"]
-  router     = var.transport["cr_name"]
+  project    = lookup(var.transport, "project", "")
+  router     = lookup(var.transport, "cr_name", "")
   ip_range   = "${cidrhost(local.tunnel_ip_range[count.index], 1)}/30"
   vpn_tunnel = google_compute_vpn_tunnel.transport[count.index].name
 }
@@ -56,7 +56,7 @@ resource "google_compute_router_peer" "squad_transport" {
   project                   = google_project.env.project_id
   router                    = google_compute_router.this.name
   peer_ip_address           = cidrhost(local.tunnel_ip_range[count.index], 1)
-  peer_asn                  = var.transport["asn"]
+  peer_asn                  = lookup(var.transport, "asn", "")
   advertised_route_priority = 100
   interface                 = google_compute_router_interface.transport[count.index].name
 }
@@ -73,8 +73,8 @@ resource "google_compute_router_interface" "squad" {
 resource "google_compute_router_peer" "transport_squad" {
   count                     = 2
   name                      = "peer-${var.name}-iface${count.index}"
-  project                   = var.transport["project"]
-  router                    = var.transport["cr_name"]
+  project                   = lookup(var.transport, "project", "")
+  router                    = lookup(var.transport, "cr_name", "")
   peer_ip_address           = cidrhost(local.tunnel_ip_range[count.index], 2)
   peer_asn                  = "645${var.asn}"
   advertised_route_priority = 100
