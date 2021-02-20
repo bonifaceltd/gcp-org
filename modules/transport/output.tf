7b6487a1bbf81d66ed721343f8ca0dccf8a20135
mrkaleslie <mrkaleslie@gmail.com>
2021-02-20 00:14:30 +0000

    utilise count on resources in squad module

--- modules/transport/output.tf
+++ modules/transport/output.tf
@@ -3,6 +3,7 @@ output "vpn" {
     project = google_project.env.project_id
     asn     = "645${var.asn}"
     gw      = google_compute_ha_vpn_gateway.this.self_link
-    cr      = google_compute_router.this.self_link
+    cr_name = google_compute_router.this.name
+    cr_link = google_compute_router.this.self_link
   }
 }
